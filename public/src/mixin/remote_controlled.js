9aa18b489298a039d7d4db58a5e9d643435319fc
sachin <sachin.saluja132@gmail.com>
2012-04-15 13:39:10 +0530

    fixed bullets

--- public/src/mixin/remote_controlled.js
+++ public/src/mixin/remote_controlled.js
@@ -1,5 +1,7 @@
 Game.mixin.RemoteControlled = {
 
+    lastBulletTime : 0,
+
     keyDown_37 : function () {
         this.model.controller.setActionLeftRight(-1); // left
     },
@@ -32,15 +34,26 @@ Game.mixin.RemoteControlled = {
         this.model.controller.setActionUpDown(0);
     },
         
+    keyHeld_32 : function () {
+        if(Date.now() - this.lastBulletTime < 200)
+            return false;
+
+        this.lastBulletTime = Date.now();
+        this.keyDown_32();
+    },
+
+
     keyDown_32 : function () {
+        this.lastBulletTime = Date.now();
+
         //fire bullet
-        var q = parseFloat(this.model.get('direction'));
+        var q = parseFloat(this.model.get('direction')) - 0.01;
         var currPos = this.model.get("currPosition");
-        var u = parseFloat(this.model.get("u"));
+        var u = parseFloat(this.model.get("u")) + 10;
 
         var c = {
-            x : parseFloat(currPos.x) + (20 * Math.cos(q)),
-            y : parseFloat(currPos.y) + (20 * Math.sin(q))
+            x : parseFloat(currPos.x) + (30 * Math.cos(q)),
+            y : parseFloat(currPos.y) + (30 * Math.sin(q))
         };
 
         Game.bullets.create({
@@ -49,5 +62,5 @@ Game.mixin.RemoteControlled = {
             q : q,
             pos : c
         });
-    }    
+    }
 };
